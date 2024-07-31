ce7efeddf69b1ef2eb020c00915d6de192732cf0
Haritha Dasanayake <hcdasanayake@gmail.com>
2024-07-31 20:45:26 +0530

    Resolved merge conflicts

--- src/db/index.ts
+++ src/db/index.ts
@@ -31,11 +31,7 @@ export { db };
 // //import { migrate } from 'drizzle-orm/postgres-js/migrator';
 // import postgres from 'postgres';
 
-// // for migrations
-// //const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
-// //migrate(drizzle(migrationClient), ...)
 
-// // for query purposes
 // const queryClient = postgres(process.env.DATABASE_URL!);
 // const db = drizzle(queryClient, {schema});
 
